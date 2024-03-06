229c82430a789e7325d5b4746b5e8019385889fb
khuongdoanngoc <khuong.dngc@gmail.com>
2024-03-06 20:23:12 +0700

    Add feature wishlist

--- client/src/pages/rooms/RoomBooking.jsx
+++ client/src/pages/rooms/RoomBooking.jsx
@@ -3,22 +3,50 @@ import Layout from "../../components/Layout/Layout";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./styles.css";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import Button from "react-bootstrap/esm/Button";
+import Rating from "@mui/material/Rating";
+
 
 function RoomBooking() {
     const location = useLocation();
     const [room, setRoom] = useState({});
+    const [checkin, setCheckin] = useState({});
+    const [checkout, setCheckout] = useState({});
+    const [total, setTotal] = useState(0);
+
+    const handleCheckinChange = (checkinTime) => {
+        if (checkout.isBefore(checkinTime)) {
+            setCheckout(checkinTime.add(1, "day"));
+        }
+        setCheckin(checkinTime);
+    };
+
+    useEffect(() => {
+        setCheckin(dayjs());
+        setCheckout(dayjs().add(1, "day"));
+    }, []);
+
+    useEffect(() => {
+        const diff = dayjs(checkout).diff(dayjs(checkin), "day");
+        setTotal(diff * room.pricePerDay);
+    }, [checkin, checkout]);
 
     useEffect(() => {
         const getRoom = async (_id) => {
             const { data } = await axios.post(
                 `${process.env.REACT_APP_API}/api/v1/product/get-product-by-id`,
                 {
-                    _id: _id,
+                    _id,
                 }
             );
             setRoom(data.product);
+            setTotal(data.product.pricePerDay);
         };
         try {
             const _idFromPathName =
@@ -32,12 +60,20 @@ function RoomBooking() {
         }
     }, []);
 
+    const disablePastDates = (date) => {
+        return date.isBefore(dayjs(), "day");
+    };
+
+    const disablePastCheckins = (date) => {
+        return date.isBefore(checkin.add(1, "day"), "day");
+    };
+
     return (
         <Layout>
             <div className="room-info">
                 <div className="room-img">
                     <img
-                        src="https://blz04pap003files.storage.live.com/y4moM-sLwjeOmGp3UaA6hu_8thvi4Zrp603qJQb5lhATZHZdwjuO_ZlCrpZD7l9738lB3qTV0td0IxjfIkvS3p9bIfZ9ufbTY5fz9O2xKjKXqV3cvPcb1y41qqSDWs3CkltiuBppEU66IRLXBbAuGvalboiXjleMOKM6ptq0CjyS0xxcF42Ly4ZxgqM_b7a93t7X5kV2QFwTtpsrXjnAHFgZm7V81f9gYTc5fpiHpeALrU?encodeFailures=1&width=1024&height=1024"
+                        src={`${room.imgUrl}`}
                         alt=""
                         style={{ width: "447.59px" }}
                     />
@@ -47,14 +83,57 @@ function RoomBooking() {
                         <h3>{room.name}</h3>
                         <hr />
                         <p>{room.description}</p>
-                        <h4>${room.price}</h4>
+                        <h4>${total}</h4>
                     </div>
                     <div className="item-time">
-                        
+                        <DatePicker
+                            className="item-picker"
+                            label={"check in"}
+                            value={checkin}
+                            onChange={handleCheckinChange}
+                            shouldDisableDate={disablePastDates}
+                        />
+                        <DatePicker
+                            className="item-picker"
+                            label={"check out"}
+                            value={checkout}
+                            onChange={(checkoutTime) =>
+                                setCheckout(checkoutTime)
+                            }
+                            shouldDisableDate={disablePastCheckins}
+                        />
                     </div>
-                    <div className="item-share"></div>
-                    <div className="item-actions"></div>
+                    <div className="item-share">
+                        <FaFacebook className="share-icons" />
+                        <FaInstagram className="share-icons" />
+                        <FaPinterest className="share-icons" />
+                        <FaTwitter className="share-icons" />
+                    </div>
+                    <div className="item-actions">
+                        <div className="btn-booking-now">
+                            <Button
+                                variant="outline-primary"
+                                id="button-addon2">
+                                Book now
+                            </Button>
+                        </div>
+                        <div className="btn-add-to-cart">
+                            <Button
+                                variant="outline-secondary"
+                                id="button-addon2">
+                                Add to cart
+                            </Button>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className="room-rates">
+                <div className="room-rating-result">
+                    <h4>Rating</h4>
+                    <Rating name="read-only" value={3} readOnly />
+                    <hr />
+                </div>
+                <div className="room-review"></div>
             </div>
         </Layout>
     );
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const { requireLogin, isAdmin } = require("../middlewares/authMiddleware");
 
-const { getAllUsers } = require("../controllers/userController");
+const { getAllUsers, getUserById } = require("../controllers/userController");
 
 router.get("/get-all-users", requireLogin, isAdmin, getAllUsers);
+router.post("/get-user-by-id", requireLogin, getUserById);
 
 module.exports = router;
